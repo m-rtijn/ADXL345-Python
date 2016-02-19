bec8ecdae94b1ebd802a93e7a80f9b26023c0e62
Tijndagamer <tijndagamer25@gmail.com>
2016-02-19 21:55:53 +0100

    Fix minor style issues

--- ADXL345.py
+++ ADXL345.py
@@ -80,7 +80,8 @@ class ADXL345:
     def set_bandwidth_rate(self, rate):
         """Changes the bandwidth rate by writing rate to BANDWIDTH_RATE_REG.
 
-        rate -- the bandwidth rate the ADXL345 will be set to. Using a pre-defined rate is advised.
+        rate -- the bandwidth rate the ADXL345 will be set to. Using a
+        pre-defined rate is advised.
         """
         try:
             self.bus.write_byte_data(self.address, self.BANDWIDTH_RATE_REG, rate)
@@ -102,10 +103,11 @@ class ADXL345:
     def set_range(self, range):
         """Changes the range of the ADXL345.
 
-        range -- the range to set the accelerometer to. Using a pre-defined range is advised.
+        range -- the range to set the accelerometer to. Using a pre-defined
+        range is advised.
         """
         value = None
-        
+
         try:
             value = self.bus.read_byte_data(self.address, self.DATA_FORMAT)
         except:
@@ -120,7 +122,8 @@ class ADXL345:
     def read_range(self, hex):
         """Reads the range the ADXL345 is currently set to.
 
-        hex -- If hex is true it wil return a hexadecimal value. If raw is false it will return a string.
+        hex -- If hex is true it wil return a hexadecimal value. If raw is false
+        it will return a string.
         """
         raw_value = self.bus.read_byte_data(self.address, self.DATA_FORMAT)
 
@@ -156,7 +159,7 @@ class ADXL345:
         x = bytes[0] | (bytes[1] << 8)
         if(x & (1 << 16 - 1)):
             x = x - (1 << 16)
-        
+
         y = bytes[2] | (bytes[3] << 8)
         if(y & (1 << 16 - 1)):
             y = y - (1 << 16)
@@ -165,13 +168,15 @@ class ADXL345:
         if(z & (1 << 16 - 1)):
             z = z - (1 << 16)
 
-        # Multiply the values by the scale multiplier to get the acceleration in g. 
+        # Multiply the values by the scale multiplier to get the acceleration
+        # in g.
         # The scale multiplier is given in the datasheet.
         x = x * self.SCALE_MULTIPLIER
         y = y * self.SCALE_MULTIPLIER
         z = z * self.SCALE_MULTIPLIER
-        
-        # Multiply the values in g by the gravity in m/s^2 to get the acceleration in m/s^2.
+
+        # Multiply the values in g by the gravity in m/s^2 to get the
+        # acceleration in m/s^2.
         x = x * self.GRAVITY_MS2
         y = y * self.GRAVITY_MS2
         z = z * self.GRAVITY_MS2
@@ -196,14 +201,14 @@ class ADXL345:
         value -- the value to be read. this can be 'x', 'y' or 'z'.
         """
         read_register = 0x00
-        
+
         if value == 'x':
             read_register = self.DATAX0
         elif value == 'y':
             read_register = self.DATAY0
         elif value == 'z':
             read_register = self.DATAZ0
-            
+
         # Read the raw bytes from the ADXL345
         bytes = self.bus.read_i2c_block_data(self.address, read_register, 2)
 
@@ -211,17 +216,19 @@ class ADXL345:
         val = bytes[0] | (bytes[1] << 8)
         if(val & (1 << 16 - 1)):
             val = val - (1 << 16)
-            
-        # Multiply the value by the scale multiplier to get the acceleration in g.
+
+        # Multiply the value by the scale multiplier to get the acceleration
+        # in g.
         val = val * self.SCALE_MULTIPLIER
 
-        # Multiply the value in g by the gravity in m/s^2 to get the acceleration in m/s^2.
+        # Multiply the value in g by the gravity in m/s^2 to get the
+        # acceleration in m/s^2.
         val = val * self.GRAVITY_MS2
-        
+
         # Round the values if the user wants to
         if round == True:
             val = round(val, 4)
-        
+
         return val
 
 # If a user runs this file just display the latest values
